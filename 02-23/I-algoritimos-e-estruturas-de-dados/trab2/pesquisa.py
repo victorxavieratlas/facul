042890f7da6bc32b0f1dcdb72c962921138f72ce
Victor Bonow Xavier <victorbonowx@gmail.com>
2023-10-21 22:16:48 -0300

    chore: add bealtiful soup

--- 02-23/I-algoritimos-e-estruturas-de-dados/trab2/pesquisa.py
+++ 02-23/I-algoritimos-e-estruturas-de-dados/trab2/pesquisa.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 import requests
 
 # requests web page
@@ -7,3 +9,13 @@ response = requests.get("https://minhaseriefavorita.com/os-100-melhores-filmes-d
 
 # obtém o código HTML da página "requisitada"
 html = response.text
+
+# faz um "parse" (conversão/ajuste) da página HTML
+soup = BeautifulSoup(html, "html.parser")
+
+# obtém apenas o texto do código do "body" da página 
+texto = soup.body.get_text().strip()
+
+print(texto[1651:4320])
+#print(texto[:-8000])
+# print(texto)
