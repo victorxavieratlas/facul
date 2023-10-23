04accc5710d90991b87bb269d0e429112624188d
Victor Bonow Xavier <victorbonowx@gmail.com>
2023-10-23 13:32:03 -0300

    chore: add extract films def

--- 02-23/I-algoritimos-e-estruturas-de-dados/trab2/pesquisa.py
+++ 02-23/I-algoritimos-e-estruturas-de-dados/trab2/pesquisa.py
@@ -7,6 +7,29 @@ def titulo(texto, sublinhado="-"):
     print(texto)
     print(sublinhado*40)
 
+def extrair_filmes(texto):
+    generos = [
+        "Ação e Aventura", "Filmes de Animações", "Filmes de comédia",
+        "Documentários", "Dramas", "Ficção Científica", "Filmes de Guerra",
+        "Filmes Históricos", "Romances", "Terror e Suspense"
+    ]
+    
+    filme_dict = {}
+    for genero in generos:
+        inicio = texto.find(genero) + len(genero)
+        fim = texto.find(generos[generos.index(genero) + 1]) if generos.index(genero) + 1 < len(generos) else None
+        filmes = texto[inicio:fim].split(")")[0:-1] if fim else texto[inicio:].split(")")
+        
+        filme_dict[genero] = []
+        for filme in filmes:
+            split_data = filme.rsplit('(', 1)
+            if len(split_data) > 1:
+                titulo = split_data[0].strip()
+                ano = int(split_data[1].replace(')', '').strip())
+                filme_dict[genero].append({"titulo": titulo, "ano": ano})
+    
+    return filme_dict
+
 response = requests.get("https://minhaseriefavorita.com/os-100-melhores-filmes-da-netflix/")
 
 # obtém o código HTML da página "requisitada"
