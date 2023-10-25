61db7fd635340eb8fd3228e21807a1f1b9afe330
Victor Bonow Xavier <victorbonowx@gmail.com>
2023-10-25 20:29:36 -0300

    chore: create users and give privileges

--- 02-23/II Banco de Dados/aula12.sql
+++ 02-23/II Banco de Dados/aula12.sql
@@ -24,3 +24,15 @@ CREATE TABLE postagem (
     FOREIGN KEY (forum_id) REFERENCES forum(id)
 )ENGINE=InnoDB;
 
+DROP USER 'moderador'@'localhost' IF EXISTS;
+CREATE USER 'moderador'@'localhost' IDENTIFIED BY '123teste';
+GRANT INSERT, UPDATE, DELETE ON aula12.usuario, aula12.forum, aula12.postagem TO 'moderador'@'localhost';
+
+DROP USER 'pikachu'@'localhost' IF EXISTS;
+CREATE USER 'pikachu'@'localhost' IDENTIFIED BY 'teste123';
+GRANT SELECT (mensagem) ON aula12.postagem TO 'pikachu'@'localhost';
+
+DROP USER 'maverick'@'localhost' IF EXISTS;
+CREATE USER 'maverick'@'localhost' IDENTIFIED BY 'topgun';
+GRANT INSERT, UPDATE, DELETE ON aula12.forum, aula12.postagem TO 'maverick'@'localhost';
+GRANT SELECT ON aula12.usuario TO 'maverick'@'localhost';
