6526e5b42810c3f97b6ed509e1c0e43fe59ef2be
Victor Bonow Xavier <victorbonowx@gmail.com>
2023-10-31 16:49:12 -0300

    chore: add load data def

---
+++ 02-23/I-algoritimos-e-estruturas-de-dados/kaggle/billionaires/billionaires.py
@@ -0,0 +1,7 @@
+
+def carrega_dados():
+    with open('forbes_2640_billionaires.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for linha in csv_reader:
+            ricos.append(linha) # lista de dicioários
+
