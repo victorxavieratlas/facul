d37dbd4afa4540c04fc5c131002b9701f001ffc7
Victor Bonow Xavier <victorbonowx@gmail.com>
2023-10-31 16:50:32 -0300

    chore: add compare country def

--- 02-23/I-algoritimos-e-estruturas-de-dados/kaggle/billionaires/billionaires.py
+++ 02-23/I-algoritimos-e-estruturas-de-dados/kaggle/billionaires/billionaires.py
@@ -22,3 +22,39 @@ def top_20():
         if contador == 20:
             break
 
+def compara_paises():
+    titulo("Comparativo dos Billionários de 2 Países")
+
+    pais1 = input("1º País: ").upper()
+    pais2 = input("2º País: ").upper()
+
+    print(f"\nBillionários do(a) {pais1}")
+
+    print("-"*40)
+
+    print("Rank Nome.......................... Atividade............ Fortuna")
+
+    conta1 = 0
+    for rico in ricos:
+        if rico['country'].upper() == pais1:
+            conta1 += 1
+            print(f"{rico['rank']:>4}. {rico['name'][0:30]:30} {rico['industry']:21} {rico['net_worth']:>5}")
+    
+    print(f"\nBillionários do(a) {pais1}: {conta1}")
+    print("-"*70)
+
+    print(f"\nBillionários do(a) {pais2}")
+
+    print("-"*40)
+
+    print("Rank Nome.......................... Atividade............ Fortuna")
+
+    conta2 = 0
+    for rico in ricos:
+        if rico['country'].upper() == pais2:
+            conta2 += 1
+            print(f"{rico['rank']:>4}. {rico['name'][0:30]:30} {rico['industry']:21} {rico['net_worth']:>5}")
+    
+    print("-"*70)
+    print(f"\nBillionários do(a) {pais2}: {conta2}")
+
