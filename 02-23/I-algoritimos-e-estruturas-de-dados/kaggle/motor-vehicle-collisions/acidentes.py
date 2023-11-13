d85a8195534bd1d2e574bcba3eded2466f506f42
victorxavieratlas <victor.xavier@atlastechnol.com>
2023-11-13 14:49:22 -0300

    chore: add overview def

--- 02-23/I-algoritimos-e-estruturas-de-dados/kaggle/motor-vehicle-collisions/acidentes.py
+++ 02-23/I-algoritimos-e-estruturas-de-dados/kaggle/motor-vehicle-collisions/acidentes.py
@@ -14,3 +14,29 @@ def titulo(texto, traco="="):
     print(texto)
     print(traco*40)
 
+def visao_geral():
+    titulo("Visão Geral")
+
+    total = len(acidentes)
+    total_feridos = 0
+    total_mortos = 0
+
+    for acidente in acidentes:
+        # se o atributo (nesta linha) tiver conteúdo
+        if acidente["NUMBER OF PERSONS INJURED"]:
+            total_feridos += int(acidente["NUMBER OF PERSONS INJURED"])
+
+        if acidente["NUMBER OF PERSONS KILLED"]:
+            total_mortos += int(acidente["NUMBER OF PERSONS KILLED"])
+    
+    # uso de separador de milhares (,)
+    # print(f"Nº Total de Acidetes: {total:,.0f}")
+
+    # uso de separador de milhares (_)
+    # print(f"Nº Total de Acidetes: {total:_.0f}")
+
+    # uso de separador de milhares (_), substituindo por (.)
+    print(f"Nº Total de Acidetes: {total:_.0f}".replace("_", "."))
+    print(f"Nº Total de Feridos: {total_feridos:,.0f}".replace("_", "."))
+    print(f"Nº Total de Mortos: {total_mortos:,.0f}".replace("_", "."))
+
