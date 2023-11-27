cc9290f8e94908a3173e9450a1a2b80fa2f9055a
victorxavieratlas <victor.xavier@atlastechnol.com>
2023-11-27 09:43:15 -0300

    chore: add load data and title def

--- 02-23/I-algoritimos-e-estruturas-de-dados/trab3/top250.py
+++ 02-23/I-algoritimos-e-estruturas-de-dados/trab3/top250.py
@@ -1,1 +1,12 @@
 import csv
+def carrega_dados():
+    with open('top250-00-19.csv', mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for linha in csv_reader:
+            transaction.append(linha) # lista de dicioários
+
+def titulo(texto, traco="="):
+    print()
+    print(texto)
+    print(traco*40)
+
