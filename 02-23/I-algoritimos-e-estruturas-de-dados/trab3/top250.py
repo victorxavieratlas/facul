75f3e5d2b1919dede9c62b5ff1123eb5df0afc02
victorxavieratlas <victor.xavier@atlastechnol.com>
2023-11-27 09:44:48 -0300

    chore: add compare league def

--- 02-23/I-algoritimos-e-estruturas-de-dados/trab3/top250.py
+++ 02-23/I-algoritimos-e-estruturas-de-dados/trab3/top250.py
@@ -63,3 +63,40 @@ def compara_4():
 
     plt.show()
 
+def compara_PL():
+
+    anos = ['2012-2013', '2013-2014', '2014-2015', '2015-2016', '2016-2017', '2017-2018']
+    temps = [0, 0, 0, 0, 0, 0]
+
+    for linha in transaction:
+        if linha["League_to"] == "Premier League":
+            if linha["Season"] == anos[0]:
+                temps[0] += 1
+            elif linha["Season"] == anos[1]:
+                temps[1] += 1
+            elif linha["Season"] == anos[2]:
+                temps[2] += 1
+            elif linha["Season"] == anos[3]:
+                temps[3] += 1
+            elif linha["Season"] == anos[4]:
+                temps[4] += 1
+            elif linha["Season"] == anos[5]:
+                temps[5] += 1
+
+    x = np.arange(6)
+
+    width = 0.2
+    # plot data in grouped manner of bar type 
+    plt.bar(x-0.3, temps[0], width, color='red') 
+    plt.bar(x-0.2, temps[1], width, color='blue')
+    plt.bar(x-0.1, temps[2], width, color='green')
+    plt.bar(x+0.1, temps[3], width, color='yellow')
+    plt.bar(x+0.2, temps[4], width, color='orange')
+    plt.bar(x+0.3, temps[5], width, color='purple')
+
+    plt.xticks(x, anos) 
+    plt.xlabel("Anos") 
+    plt.ylabel("Quantidade de transferências") 
+    plt.legend([anos[0], anos[1], anos[2], anos[3], anos[4], anos[5]]) 
+    plt.show() 
+
