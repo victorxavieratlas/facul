ab638a57951fe3fed7feec9078b2c942b91ed963
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-03-14 21:52:34 -0300

    feat: add layout

---
+++ 24-01/Linguagens-Programacao-Emergentes/cineclube/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Titulo from "./components/Titulo";
+
+export const metadata: Metadata = {
+  title: "CineClube Avenida",
+  description: "Clube de Cinema, aluguel e avaliação de filmes",
+  keywords: ["Cinema", "Filmes", "Locação de Filmes"]
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <head>
+       <link rel="shortcut icon" href="./logo.png" type="image/x-icon" />
+      </head>
+      <body>
+        <Titulo />
+        {children}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+      </body>
+    </html>
+  );
+}
