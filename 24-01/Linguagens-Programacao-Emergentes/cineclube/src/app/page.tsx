8a572aa71bb270bd2c2b733caf781d3652096514
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-03-17 22:30:10 -0300

    feat: add more movie

--- 24-01/Linguagens-Programacao-Emergentes/cineclube/src/app/page.tsx
+++ 24-01/Linguagens-Programacao-Emergentes/cineclube/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       genero: "Animação",
       preco: 14.99,
       duracao: 95,
-      foto: "https://static.wikia.nocookie.net/dublagem/images/2/2f/Meu_Malvado_Favorito_4.jpg/revision/latest?cb=20240209004154&path-prefix=pt-br",
+      foto: "https://cinepop.com.br/wp-content/uploads/2024/02/meumalvadofavorito4_2.jpg.webp",
     },
     {
       id: 2,
@@ -19,11 +19,35 @@ export default function Home() {
       preco: 10.99,
       duracao: 92,
       foto: "https://cinemacomrapadura.com.br/imagens/2012/08/Cartaz-Nacional-Hoteltransilvania-CCR.jpg",
+    },
+    {
+      id: 3,
+      titulo: "Garfild Fora de Casa",
+      genero: "Animação",
+      preco: 18.99,
+      duracao: 103,
+      foto: "https://cinepop.com.br/wp-content/uploads/2024/01/garfield.jpg.webp",
+    },
+    {
+      id: 4,
+      titulo: "Kung Fu Panda 4",
+      genero: "Animação",
+      preco: 19.99,
+      duracao: 120,
+      foto: "https://cinepop.com.br/wp-content/uploads/2024/01/kung-fu-panda-2.jpg.webp",
+    },
+    {
+      id: 5,
+      titulo: "Elementos",
+      genero: "Animação",
+      preco: 19.99,
+      duracao: 120,
+      foto: "https://cinepop.com.br/wp-content/uploads/2023/05/imagem_2023-05-03_140718851.jpg.webp",
     }
 ]
   return (
     <div className="max-w-7xl mx-auto mt-4">
-      <Carousel filme={filme[1]}/>
+      <Carousel />
 
 
       <h1 className="my-4 mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-200 lg:text-3xl">Alugue Filmes: </h1>
@@ -31,10 +55,14 @@ export default function Home() {
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 my-4">
         <ItemFilme filme={filme[0]}/>
         <ItemFilme filme={filme[1]}/>
-        <ItemFilme filme={filme[0]}/>
+        <ItemFilme filme={filme[2]}/>
+        <ItemFilme filme={filme[3]}/>
+        <ItemFilme filme={filme[4]}/>
+        <ItemFilme filme={filme[4]}/>
+        <ItemFilme filme={filme[3]}/>
+        <ItemFilme filme={filme[2]}/>
         <ItemFilme filme={filme[1]}/>
         <ItemFilme filme={filme[0]}/>
-        <ItemFilme filme={filme[1]}/>
       </div>
     </div>
   )
