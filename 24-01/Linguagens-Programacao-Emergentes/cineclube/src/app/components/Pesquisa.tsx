11a14a177ed70a728ce4573e92b7d5b65b15e4db
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-03-17 22:32:12 -0300

    style: update search

--- 24-01/Linguagens-Programacao-Emergentes/cineclube/src/app/components/Pesquisa.tsx
+++ 24-01/Linguagens-Programacao-Emergentes/cineclube/src/app/components/Pesquisa.tsx
@@ -1,6 +1,6 @@
 function Pesquisa() {
     return (
-        <div className="flex">
+        <div className="flex justify-content align-center w-full pt-2 ">
             <form className="flex-1">   
                 <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Pesquisar</label>
                 <div className="relative">
@@ -9,10 +9,14 @@ function Pesquisa() {
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-200 bg-gray-800" placeholder="Título ou Gênero do filme..." required />
-                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5  bg-purple-500 hover:bg-purple-600 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2">Pesquisar</button>
-                    {/* colocar botões um do lado do outro e retirar borda ao escrever no input */}
-                    <button type="button" className="text-white absolute end-2.5 bottom-2.5  bg-purple-500 hover:bg-purple-600 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2">Ver Todos</button>
+                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-200 bg-gray-900" placeholder="Título ou Gênero do filme..." required />
+                    <div className="inline justify-content align-center">
+                        <button type="submit" className="h-10 w-28 text-white absolute end-2.5 bottom-2.5 mx-32 bg-purple-500 hover:bg-purple-600 font-medium rounded-lg text-sm px-4 py-2">Pesquisar</button>
+                        {/* colocar botões um do lado do outro e retirar borda ao escrever no input */}
+                        <button type="button" className="h-10 w-28 text-white absolute end-2.5 bottom-2.5 border-solid border-2 border-purple-500 bg-transparent hover:border-purple-600 hover:bg-purple-600 font-medium rounded-lg text-sm px-4 py-2">Ver Todos</button>
+                    </div>
+                </div>
+                <div className="absolute ">
                 </div>
             </form>
         </div>
