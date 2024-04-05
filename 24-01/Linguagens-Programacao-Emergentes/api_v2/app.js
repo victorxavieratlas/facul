49575c2917b9acf3dceb63a01daade014d38c5b8
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-04-04 21:19:05 -0300

    chore: create express app api

---
+++ 24-01/Linguagens-Programacao-Emergentes/api_v2/app.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from "cors"
+import { sequelize } from './database/conecta.js'
+import { Filme } from './models/Filme.js'
+import { Cliente } from './models/Cliente.js'
+import routes from './routes.js'
+
+const app = express()
+const port = 3004
+
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+app.get('/', (req, res) => {
+  res.send('Sistema de Controle de Filmes')
+})
+
+async function conecta_db() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com Banco de Dados realizada com Sucesso');
+    // await Filme.sync({alter: true})      // RECRIA A TABELA: cria a tabela no banco (se não existir)
+    await Filme.sync()      // cria a tabela no banco (se não existir)
+    console.log("Tabela de Filmes: Ok")
+    await Cliente.sync()      // cria a tabela no banco (se não existir)
+    console.log("Tabela de Clientes: Ok")
+  } catch (error) {
+    console.error('Erro ao conectar o banco de dados:', error);
+  }  
+}
+conecta_db()
+
+app.listen(port, () => {
+  console.log(`API de Filmes Rodando na Porta: ${port}`)
+})
