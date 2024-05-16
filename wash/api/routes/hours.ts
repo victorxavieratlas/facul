e9597c7ffe865aa3cfa97d0181c28216dc937d6a
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-05-15 22:03:41 -0300

    feat(routes): update working hours router

--- wash/api/routes/hours.ts
+++ wash/api/routes/hours.ts
@@ -8,6 +8,7 @@ type WorkingHourInput = {
     start: string;
     end: string;
     scheduleId: number;
+    profileId: number;
   };
 
 router.get("/", async (req, res) => {
@@ -59,12 +60,31 @@ router.get("/schedule/:scheduleId", async (req, res) => {
     }
 })
 
+router.get("/profile/:profileId", async (req, res) => {
+    const { profileId } = req.params
+
+    try {
+        const workingHours = await prisma.workingHour.findMany({
+            where: {
+                profileId: Number(profileId)
+            },
+            include: {
+                schedule: true,
+            }
+        })
+        res.status(200).json(workingHours)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
 //Adicionar horário de almoço
 router.post("/", async (req, res) => {
     const {
         start,
         end,
-        scheduleId
+        scheduleId,
+        profileId
     } = req.body
 
     try {
@@ -72,7 +92,8 @@ router.post("/", async (req, res) => {
             data: {
                 start,
                 end,
-                scheduleId
+                scheduleId,
+                profileId
             }
         })
         res.status(201).json(workingHour)
@@ -85,12 +106,13 @@ router.post("/create", async (req, res) => {
     const workingHours: WorkingHourInput[] = req.body.workingHours;
 
     try {
-        const creates = workingHours.map(({ start, end, scheduleId }) => 
+        const creates = workingHours.map(({ start, end, scheduleId, profileId }) => 
             prisma.workingHour.create({
                 data: {
                     start,
                     end,
-                    scheduleId
+                    scheduleId,
+                    profileId
                 }
             })
         );
