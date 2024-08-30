2aae1d05ce133eb60efdd83537ca71e2ca6b0d07
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-08-30 09:20:12 -0300

    feat(all): add new features

--- wash/web-wash/src/app/components/ServicesAccordion.tsx
+++ wash/web-wash/src/app/components/ServicesAccordion.tsx
@@ -1,8 +1,3 @@
-//
-//Ajustar o accordion
-//
-
-
 import { useState } from 'react';
 
 interface Service {
@@ -23,6 +18,7 @@ export default function ServicesAccordion({ services, profileId, updateServices 
     const [showAddForm, setShowAddForm] = useState(false);
     const [newService, setNewService] = useState({ title: '', description: '', value: '', time: '' });
     const [editingService, setEditingService] = useState<Service | null>(null);
+    const [expandedId, setExpandedId] = useState<number | null>(null);
 
     const handleAddService = async () => {
         const response = await fetch(`http://localhost:3007/services`, {
@@ -31,13 +27,12 @@ export default function ServicesAccordion({ services, profileId, updateServices 
             body: JSON.stringify({ profileId, ...newService })
         });
         if (response.ok) {
-            updateServices(profileId.toString());
+            await updateServices(profileId.toString());
             setShowAddForm(false);
             setNewService({ title: '', description: '', value: '', time: '' }); // Reset the form
         }
     };
 
-    //Ajustar update
     const handleUpdateService = async (id: number) => {
         const response = await fetch(`http://localhost:3007/services/${id}`, {
             method: 'PUT',
@@ -45,18 +40,25 @@ export default function ServicesAccordion({ services, profileId, updateServices 
             body: JSON.stringify(editingService)
         });
         if (response.ok) {
-            updateServices(profileId.toString());
+            await updateServices(profileId.toString());
             setEditingService(null);
         }
     };
 
-    //Ajustar delete
     const handleDeleteService = async (id: number) => {
         const response = await fetch(`http://localhost:3007/services/${id}`, {
             method: 'DELETE'
         });
         if (response.ok) {
-            updateServices(profileId.toString());
+            await updateServices(profileId.toString());
+        }
+    };
+
+    const toggleExpand = (id: number) => {
+        if (expandedId === id) {
+            setExpandedId(null);
+        } else {
+            setExpandedId(id);
         }
     };
 
@@ -64,21 +66,32 @@ export default function ServicesAccordion({ services, profileId, updateServices 
         <div className="p-4 bg-white shadow rounded-lg">
             {services.length > 0 ? (
                 services.map((service) => (
-                    <div key={service.id} className="border-b p-4">
-                        <div className="flex justify-between items-center">
-                            <h3 className="text-lg font-semibold">{service.title}</h3>
-                            <div>
-                                <button className="text-blue-500 hover:text-blue-700 px-2" onClick={() => setEditingService(service)}>Edit</button>
-                                <button className="text-red-500 hover:text-red-700 px-2" onClick={() => handleDeleteService(service.id)}>Delete</button>
+                    <div key={service.id} className="border-t border-gray-200">
+                        <button className="flex items-center justify-between w-full p-5 font-medium text-gray-500 hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 rounded-t-lg" onClick={() => toggleExpand(service.id)}>
+                            <span>{service.title}</span>
+                            <div className='flex items-center text-left'>
+                                <button className="text-blue-600 hover:text-blue-800 px-2" onClick={(e) => { e.stopPropagation(); setEditingService(service); }}>Edit</button>
+                                <button className="text-red-600 hover:text-red-800 px-2" onClick={(e) => { e.stopPropagation(); handleDeleteService(service.id); }}>Delete</button>
+                                <p>R$ </p>
+                                <p className=''> {service.value}.00</p>
+                                <svg className={`w-4 h-4 transform transition-transform ${expandedId === service.id ? 'rotate-180' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                                </svg>
                             </div>
-                        </div>
+                        </button>
+                        {expandedId === service.id && (
+                            <div className="p-5">
+                                <p>{service.description}</p>
+                                <p>{service.time}</p>
+                            </div>
+                        )}
                         {editingService && editingService.id === service.id && (
-                            <div className="mt-4">
+                            <div className="p-5">
                                 <input className="border p-1 rounded mr-2" value={editingService.title} onChange={(e) => setEditingService({ ...editingService, title: e.target.value })} placeholder="Title" />
                                 <input className="border p-1 rounded mr-2" value={editingService.description} onChange={(e) => setEditingService({ ...editingService, description: e.target.value })} placeholder="Description" />
                                 <input className="border p-1 rounded mr-2" value={editingService.value} onChange={(e) => setEditingService({ ...editingService, value: e.target.value })} placeholder="Value" />
                                 <input className="border p-1 rounded" value={editingService.time} onChange={(e) => setEditingService({ ...editingService, time: e.target.value })} placeholder="Time" />
-                                <button className="ml-2 bg-blue-500 hover:bg-blue-700 text-white p-1 rounded" onClick={() => handleUpdateService(service.id)}>Update</button>
+                                <button className="ml-2 bg-blue-600 hover:bg-blue-800 text-white p-1 rounded" onClick={() => handleUpdateService(service.id)}>Update</button>
                             </div>
                         )}
                     </div>
@@ -88,15 +101,15 @@ export default function ServicesAccordion({ services, profileId, updateServices 
             )}
 
             {showAddForm ? (
-                <div className="mt-4">
+                <div className="p-5">
                     <input className="border p-1 rounded mr-2" value={newService.title} onChange={(e) => setNewService({ ...newService, title: e.target.value })} placeholder="Title" />
                     <input className="border p-1 rounded mr-2" value={newService.description} onChange={(e) => setNewService({ ...newService, description: e.target.value })} placeholder="Description" />
                     <input className="border p-1 rounded mr-2" value={newService.value} onChange={(e) => setNewService({ ...newService, value: e.target.value })} placeholder="Value" />
                     <input className="border p-1 rounded" value={newService.time} onChange={(e) => setNewService({ ...newService, time: e.target.value })} placeholder="Time" />
-                    <button className="ml-2 bg-blue-500 hover:bg-blue-700 text-white p-1 rounded" onClick={handleAddService}>Add Service</button>
+                    <button className="ml-2 bg-blue-600 hover:bg-blue-800 text-white p-1 rounded" onClick={handleAddService}>Add Service</button>
                 </div>
             ) : (
-                <button className="mt-4 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded" onClick={() => setShowAddForm(true)}>Add Service</button>
+                <button className="mt-4 bg-green-600 hover:bg-green-800 text-white py-2 px-4 rounded" onClick={() => setShowAddForm(true)}>Add Service</button>
             )}
         </div>
     );
